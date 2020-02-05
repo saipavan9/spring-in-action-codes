bfb73c887aad17f3afa5b5fd28bf7fc15108d1c3
NU20102401 <NU20102401@T-113190033.wipro.com>
2020-02-05 16:16:28 +0530

    Chapter-3 JPA

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,22 +1,41 @@
 package tacos;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
 
-	  // end::allButValidation[]
+	
+	  @Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+	  private Long id;
+	  
 	  @NotNull
 	  @Size(min=5, message="Name must be at least 5 characters long")
-	  // tag::allButValidation[]
 	  private String name;
-	  // end::allButValidation[]
+
+	  private Date createdAt;
+	  
+	  @ManyToMany(targetEntity = Ingredient.class)
 	  @Size(min=1, message="You must choose at least 1 ingredient")
-	  // tag::allButValidation[]
-	  private List<String> ingredients;
+	  private List<Ingredient> ingredients;
+	  
+	  @PrePersist
+	  void createdAt() {
+		  this.createdAt = new Date();
+	  }
+	  
 }
